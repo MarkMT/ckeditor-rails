549cff603aaff219b2375e5f2365a7e35805f6b0
MarkMT <nzl216@gmail.com>
2024-10-07 16:57:30 +1300

    Check call stack

--- lib/ckeditor-rails/asset_url_processor.rb
+++ lib/ckeditor-rails/asset_url_processor.rb
@@ -17,22 +17,23 @@ module Ckeditor
       def self.call(input)
         return { data: input[:data] } unless stylesheet_files.include?(input[:filename])
 
-        puts "***** input filename #{input[:filename].inspect}"
-        puts "***** input environment #{input[:environment].inspect}"
-        puts "***** context_class #{input[:environment].context_class.inspect}"
-        puts "***** context_class #{input[:environment].context_class.ancestors.inspect}"
+        puts caller(0)
+        #puts "***** input filename #{input[:filename].inspect}"
+        #puts "***** input environment #{input[:environment].inspect}"
+        #puts "***** context_class #{input[:environment].context_class.inspect}"
+        #puts "***** context_class #{input[:environment].context_class.ancestors.inspect}"
         
         context = input[:environment].context_class.new(input)
-        puts "***** context #{context.inspect}"
+        #puts "***** context #{context.inspect}"
         path_prefix = assets_base_path()
-        puts "***** path_prefix #{path_prefix.inspect}"
+        #puts "***** path_prefix #{path_prefix.inspect}"
         matched_folders = input[:filename].match(/\/ckeditor\/(plugins|skins)\/([\w-]+)\//)
 
-        puts "***** input data #{input[:data].inspect}"
+        #puts "***** input data #{input[:data].inspect}"
         data = input[:data].gsub(REGEX) { |_match|
-          puts "***** $1 #{$1.inspect}"
+          #puts "***** $1 #{$1.inspect}"
           raw_asset_path = context.asset_path($1)
-          puts "***** raw_asset_path #{raw_asset_path.inspect}"
+          #puts "***** raw_asset_path #{raw_asset_path.inspect}"
           if raw_asset_path.starts_with?(path_prefix)
             "url(#{raw_asset_path})"
           elsif matched_folders
